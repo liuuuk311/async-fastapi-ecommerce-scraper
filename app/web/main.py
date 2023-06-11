e7c79c541407e76f47fd2c795fbcf6ade7221229
luca <lucapalonca@gmail.com>
2023-06-11 22:53:24 +0200

    CORS

--- app/web/main.py
+++ app/web/main.py
@@ -1,9 +1,15 @@
+import logging
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from web.api.v1.router import api_router
 from web.core.config import settings
 from web.models.generics import HealthCheck
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
@@ -22,10 +28,16 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
+logger.info(f"CORS ORIGIN: {settings.BACKEND_CORS_ORIGINS}")
+
 
 @app.get("/", response_model=HealthCheck, tags=["status"])
 async def health_check():
-    return {"name": settings.PROJECT_NAME, "version": settings.VERSION, "status": "ok"}
+    return {
+        "name": settings.PROJECT_NAME,
+        "version": settings.VERSION,
+        "status": "ok",
+    }
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
